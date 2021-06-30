984200c2889bc45712044b5714bee191f46baf00
JessiJoya <jessica.joyaq@gmail.com>
2021-06-30 12:20:31 -0500

    use una promesa para resolver el metodo  fs.readFile

--- createobject.js
+++ createobject.js
@@ -1,52 +1,54 @@
 const path = require('path');
 const fs = require('fs')
 
+const dirPath = path.resolve(__dirname); // encuentro el path actual
 
+const findFilesMd = (inputPath) => {
 
+    let files = fs.readdirSync(inputPath);
+    const extFileMD = (file) => { return path.extname(file).toLowerCase() === '.md' }
+    const mdFilesList = files.filter(extFileMD);
 
-const dirPath = path.resolve(__dirname); // path to your directory goes here
-
-const findFilesMd=(inputPath)=>{
-    
-let files = fs.readdirSync(inputPath);
-const extFileMD=(file)=>{return path.extname(file).toLowerCase() === '.md'}
-const mdFilesList = files.filter(extFileMD);
-return mdFilesList
+    return mdFilesList
 }
-const arrFilesMD=findFilesMd(dirPath)
+
+const arrFilesMD = findFilesMd(dirPath)
 
 
-const findLinks = (filesMD) =>{
-    
-    let contentFile=fs.readFileSync(filesMD, 'utf-8')
-    
-    const expRegLinks =/(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])/igm;
-    
-    
-    const listLinks = [...contentFile.match(expRegLinks)];
+const findLinks = (filesMD) => {
+
+    let contentFile = fs.readFileSync(filesMD, 'utf-8')
+
+    const expRegLinks = /(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])/igm;
+
+
+    const listLinks = [...contentFile.match(expRegLinks)];//recorrerlo map retund listlinks.map(){retorno el objeto}
+    // console.log(listLinks)
     let objectlinks = [];
-    for (let i=0; i<listLinks.length;i++){
-    
-    let indice = contentFile.indexOf(listLinks[i])
-    let sliceIndice = contentFile.substr(0,indice);
-    let indiceApertura = sliceIndice.lastIndexOf("[")+1
-    let indicecierre = sliceIndice.lastIndexOf("]")
-    // console.log(sliceIndice)
-    // console.log(indice)
+    for (let i = 0; i < listLinks.length; i++) {
 
-    let objectlink =
-    {
-        link : listLinks[i],
-        href : filesMD,
-        text : contentFile.substring(indiceApertura,indicecierre)
+        let indice = contentFile.indexOf(listLinks[i])
+        let sliceIndice = contentFile.substr(0, indice);
+        let indiceApertura = sliceIndice.lastIndexOf("[") + 1;
+        let indicecierre = sliceIndice.lastIndexOf("]");
+        // console.log(sliceIndice)
+        // console.log(indice)
+
+        let objectlink =
+        {
+            link: listLinks[i],
+            href: filesMD,
+            text: contentFile.substring(indiceApertura, indicecierre)
+        }
+        objectlinks.push(objectlink)
     }
-    objectlinks.push(objectlink)
-}
-   return objectlinks;
+    return objectlinks;
 
 }
 
-const arrayLinks =arrFilesMD.flatMap(md=>findLinks(md));
+
+
+const arrayLinks = arrFilesMD.flatMap(md => findLinks(md));
 console.log(arrayLinks)
 
 // let mdLinks = [];
--- createobjectcll.js
+++ createobjectcll.js
@@ -6,44 +6,67 @@ const fs = require('fs')
 
 const dirPath = path.resolve(__dirname); // path to your directory goes here
 let files = fs.readdirSync(dirPath);
-const extFileMD=(file)=>{return path.extname(file).toLowerCase() === '.md'}
+const extFileMD = (file) => { return path.extname(file).toLowerCase() === '.md' }
 mdFilesList = files.filter(extFileMD);
-console.log(mdFilesList)
+
+// https://stackoverflow.com/questions/44199883/how-do-i-get-a-list-of-files-with-specific-file-extension-using-node-js
+// const dirpath = path.join(__dirname, '/path')
+
+// fs.readdir(dirpath, function(err, files) {
+//   const txtFiles = files.filter(el => path.extname(el) === '.txt')
+//   // do something with your files, by the way they are just filenames...
+// })
+
+// console.log(mdFilesList)
 
 
 
-const arrLinks =[];
+const arrLinks = [];
 //////////////////////////////////////////////////////////////////////////////////////////////leer el archivo indicado
-const leerMD =(ruta)=>{
+const leerMD = (ruta) => {
+  //creando una instancia 
+  return new Promise ((resolve,reject)=>{
   
-fs.readFile(ruta, 'utf8' , (err, data) => {
-  if (err) {
-    console.error(err)
-    return
-  }
- const expRegLinks =/(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])/igm;
- const str = data;
-
-const arrayLinks = [...str.match(expRegLinks)];
-console.log(arrayLinks.length)
-console.log(typeof arrayLinks)
-let objectlinks = [];
-for (let i = 0; i < arrayLinks.length; i++) {
-let arrObje = 
-  {
-    link: arrayLinks[i],
-    href: ruta
-  }
-  objectlinks.push(arrObje);
-}
-// console.log(objectlinks) //como lo saco de aqui!
-return objectlinks
-})
-// console.log(objectlinks) //como lo saco de aqui!
+    fs.readFile(ruta, 'utf8', (err, data) => {
+      if (err) {
+        reject(err)
+        return 
+      }
+      // console.log(data);
+      const expRegLinks = /(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])/igm;
+      const listLinks = [...data.match(expRegLinks)];//recorrerlo map retund listlinks.map(){retorno el objeto}
+      // console.log(listLinks)
+      let objectlinks = [];
+      for (let i = 0; i < listLinks.length; i++) {
+  
+          let indice = data.indexOf(listLinks[i])
+          let sliceIndice = data.substr(0, indice);
+          let indiceApertura = sliceIndice.lastIndexOf("[") + 1;
+          let indicecierre = sliceIndice.lastIndexOf("]");
+          // console.log(sliceIndice)
+          // console.log(indice)
+  
+          let objectlink =
+          {
+              link: listLinks[i],
+              href: ruta,
+              text: data.substring(indiceApertura, indicecierre)
+          }
+          objectlinks.push(objectlink)
+      }
+      // console.log(objectlinks) //como lo saco de aqui!
+      resolve(objectlinks) 
+    })
+  })
+ 
 }
 // console.log(arrLinks)
+//consuminla promesa con .then
+leerMD('READssssME.md')
+.then((data)=>{console.log(data)})
+.catch((error)=>{console.log(error)})
 
-mdFilesList.forEach(element => leerMD(element));
+// mdFilesList.forEach(element => leerMD(element));
 
 
 
@@ -62,7 +85,7 @@ mdFilesList.forEach(element => leerMD(element));
 
 // ejemplo de una funcion recursiva
 // let cuentaAtras = (numero) => {
-  
+
 //   if (numero === 0) {
 //       return;
 //   }
